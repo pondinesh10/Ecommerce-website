6b50033c7d8fd3486ed4ec530b75a16d14edbf21
pondinesh10 <140954617+pondinesh10@users.noreply.github.com>
2023-07-30 18:01:46 +0530

    chatbot

---
+++ chat/mscbot/static/scripts/responses.js
@@ -0,0 +1,22 @@
+function getBotResponse(input) {
+    //rock paper scissors
+    if (input == "rock") {
+        return "paper";
+    } else if (input == "paper") {
+        return "scissors";
+    } else if (input == "scissors") {
+        return "rock";
+    }
+
+    // Simple responses
+    if (input == "hello") {
+        return "Hello there!";
+    } else if (input == "goodbye") {
+        return "Talk to you later!";
+    } else if (input == "hai"){
+        return "hai!";
+    }
+      else if(input == "hi"){
+        return "Hello there!";
+    }
+}
